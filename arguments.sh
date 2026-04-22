2c0c476906c4e18b92c04e63e52115d38f2128f2
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-22 16:21:33 +0530

    shell

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:Hello $PERSON2, Good morning"
+echo "$PERSON2:Hii $PERSON1, Good Morning, How are you"
+echo "$PERSON1:I am good, Thank You and what about you"
+echo "$PERSON2:I am superub, Thank you very much"
