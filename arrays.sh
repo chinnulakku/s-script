488b457fe835fed03793571a9105b1ad7235d655
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-22 17:01:03 +0530

    shell

---
+++ arrays.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+COLORS=("Red" "Green" "Maroon" "Blue" "Orange")
+
+echo " First color: ${COLORS[0]}"
+
+echo " Second color: ${COLORS[1]}"
+
+echo " Third color: ${COLORS[2]}"
+
+echo " Fourth color: ${COLORS[3]}"
+
+echo " Fifth color: ${COLORS[4]}"
+
+echo "All Colors: ${COLORS[@]}
