03767cb6a98e204065448e48f3dd896c2ca0b082
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-22 15:48:01 +0530

    shell

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+PERSON1=Aruna
+PERSON2=sudha
+
+echo "PERSON1:Hello sudha, Good morning"
+echo "PERSON2:Hii, Good Morning, How are you"
+echo "PERSON1:I am good, Thank You and what about you"
+echo "PERSON2:I am superub, Thank you very much"
