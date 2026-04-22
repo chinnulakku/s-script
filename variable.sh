a34e8564d7a924999918d30eab82dce53700eeb1
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-22 16:05:05 +0530

    shell

--- variable.sh
+++ variable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash/
 
-PERSON1=Aruna
-PERSON2=sudha
+PERSON1=Chinnu
+PERSON2=Lakku
 
-echo "PERSON1:Hello sudha, Good morning"
-echo "PERSON2:Hii, Good Morning, How are you"
-echo "PERSON1:I am good, Thank You and what about you"
-echo "PERSON2:I am superub, Thank you very much"
+echo "$PERSON1:Hello $PERSON2, Good morning"
+echo "$PERSON2:Hii $PERSON1, Good Morning, How are you"
+echo "$PERSON1:I am good, Thank You and what about you"
+echo "$PERSON2:I am superub, Thank you very much"
