868ef1bc8cfb9fb143e7138006f54bfde1b14b20
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-25 15:31:33 +0530

    shell

---
+++ install-mysql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR:Please run this script with the root access"
+    exit 1 # you can give  other than 0
+else
+    echo " you are root users"
+fi #fi means reverse of if, indicating condition end
+
+yun install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MYSQL is failed"
+    exit 1
+else
+    echo " Installing MYSQL is success"
+fi
+
+yum install git -y
+
+if [ $ID -ne 0]
+then 
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is SUCCESS"
+fi
