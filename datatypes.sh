785dd6137ae1cdc8b15ce0f0fa4f68029d16c336
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-25 13:07:12 +0530

    shell

--- datatypes.sh
+++ datatypes.sh
@@ -3,6 +3,12 @@
 NUMBER1=$1
 NUMBER2=$2
 
-SUM=$(($NUMBER1 + $NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Total:: $SUM"
+
+echo " How many args passed : :$#"
+
+echo " All args passed: : $@"
+
+echo "Script name: : $0
