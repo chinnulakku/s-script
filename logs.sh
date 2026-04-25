5617dadfc47827b6075ddfaa39db910d12ef8a4a
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-25 16:30:19 +0530

    shell

---
+++ logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> LOGFILE
+
+VALIDATE() {
+    if ($1 -ne 0)
+    then 
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+    else
+        echo -e "$2 ...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with the root access $N"
+    exit 1 # you can give other than 0
+else 
+    echo "You are root user"
+fi  # fi means reverse of if, indicating condition end
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing GIT"
