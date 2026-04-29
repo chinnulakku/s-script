a245b8c0b587890d733b40ef3d2c5fb73e556098
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-29 05:47:19 +0530

    s-scripts

--- install-packages.sh
+++ install-packages.sh
@@ -22,7 +22,7 @@ VALIDATE(){
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R ERROR:: please run this script with root access"
+    echo -e "$R ERROR:: please run this script with root access $N"
     exit 1 # you can other than 0
 else
     echo "you are root user"
