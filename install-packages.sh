d7973b9cef4a7877fd0a9f3890d49d314f05815e
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-29 06:05:27 +0530

    s-scripts

--- install-packages.sh
+++ install-packages.sh
@@ -37,7 +37,7 @@ do
     yum list installed $package #check installed or not
     if [ $? -ne 0] #if not installed
     then 
-        yum install $package -y # install the package
+        yum install $package -y &>> $LOGFILE # install the package
         VALIDATE $? "Installation of $package" # validate
     else
         echo -e "$package is already installed ... $Y SKIPPING $N"
